922f492b2c2084e3796144f376fbd30086544eb6
今吉恒介 <ikkorokoro@icloud.com>
2021-05-05 23:13:34 +0900

    セッターを用いてのパラメータの設定

--- main.rb
+++ main.rb
@@ -1,3 +1,28 @@
 class Brave
 
+  #nameのセッター
+  def name=(name)
+    @name = name
+  end
+
+  #hpのセッター
+  def hp=(hp)
+    @hp = hp
+  end
+
+  #offenseのセッター
+  def offense=(offense)
+    @offense = offense
+  end
+
+  #defenseのセッター
+  def defense=(defense) 
+    @defense = defense
+  end   
+
+  brave = Brave.new
+  brave.name = "テリー"
+  brave.hp = 500
+  brave.offense = 150
+  brave.defense = 100
 end
