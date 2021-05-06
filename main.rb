0991158776d1be83ae08d87575f9384d09174065
今吉恒介 <ikkorokoro@icloud.com>
2021-05-06 23:15:23 +0900

    柔軟にパラメータの初期値を設定できるよう、initializeメソッドをさらに改善 引数に**を記述：ハッシュしか受け取れなくなる

--- main.rb
+++ main.rb
@@ -1,11 +1,12 @@
 class Brave
 
-  # initializeメソッドを定義
-  def initialize
-    @name = "テリー"
-    @hp = 500
-    @offense = 150
-    @defense = 100
+
+  # 引数に**を記述：ハッシュしか受け取れなくなる
+  def initialize(**params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
   end
 
   #インスタンス作成時にinitializeメソッドが呼び出され初期値を設定するためセッター不要のためを削除
@@ -48,7 +49,7 @@ class Brave
     @defense
   end
 
-  brave = Brave.new
+  brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
   # インスタンス作成時にinitializeメソッドが呼び出され初期値を設定するため以下不要のためを削除
   # brave.name = "テリー"
   # brave.hp = 500
@@ -56,8 +57,8 @@ class Brave
   # brave.defense = 100
 
 
-  # putsや文字列を囲む""（ダブルクウォーテーション）を何度も書く必要がなくなります。
-  # 複数行の文字列を書くときにはこちらの方が楽なのでぜひ使ってみてください。
+  #puts <<~TEXT (putsや文字列を囲む""（ダブルクウォーテーション）を何度も書く必要がなくなります。
+  # 複数行の文字列を書くときにはこちらの方が楽なのでぜひ使ってみてください。)
   puts <<~TEXT
   NAME：#{brave.name}
   HP：#{brave.hp}
