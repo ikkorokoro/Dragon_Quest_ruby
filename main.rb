8ccb299084cd0fb2a41c1eb5168a707f6346514a
今吉恒介 <ikkorokoro@icloud.com>
2021-05-06 22:49:21 +0900

    ゲッターを設定 ヒアドキュメント記法によりputs、""を簡略化

--- main.rb
+++ main.rb
@@ -5,9 +5,19 @@ class Brave
     @name = name
   end
 
+  #nameのゲッター
+  def name
+    @name
+  end
+
   #hpのセッター
   def hp=(hp)
     @hp = hp
+  end
+
+  #hpのゲッター
+  def hp
+    @hp
   end
 
   #offenseのセッター
@@ -15,14 +25,33 @@ class Brave
     @offense = offense
   end
 
+  #offenseのゲッター
+  def offense
+    @offense
+  end
+
   #defenseのセッター
   def defense=(defense) 
     @defense = defense
   end   
+
+  #defenseのゲッター
+  def defense
+    @defense
+  end
 
   brave = Brave.new
   brave.name = "テリー"
   brave.hp = 500
   brave.offense = 150
   brave.defense = 100
+
+  # putsや文字列を囲む""（ダブルクウォーテーション）を何度も書く必要がなくなります。
+  # 複数行の文字列を書くときにはこちらの方が楽なのでぜひ使ってみてください。
+  puts <<~TEXT
+  NAME：#{brave.name}
+  HP：#{brave.hp}
+  OFFENSE：#{brave.offense}
+  DEFENSE：#{brave.defense}
+  TEXT
 end
