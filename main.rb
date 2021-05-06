e1883ff1c85700ee60b1da4de51fc74d08a4e9a2
今吉恒介 <ikkorokoro@icloud.com>
2021-05-06 23:05:05 +0900

    initializeメソッド定義によりセッターを削除し、よりコードを短縮

--- main.rb
+++ main.rb
@@ -1,50 +1,60 @@
 class Brave
 
-  #nameのセッター
-  def name=(name)
-    @name = name
+  # initializeメソッドを定義
+  def initialize
+    @name = "テリー"
+    @hp = 500
+    @offense = 150
+    @defense = 100
   end
 
-  #nameのゲッター
+  #インスタンス作成時にinitializeメソッドが呼び出され初期値を設定するためセッター不要のためを削除
+  # def name=(name)
+  #   @name = name
+  # end
+
+  #ゲッター
   def name
     @name
   end
 
-  #hpのセッター
-  def hp=(hp)
-    @hp = hp
-  end
+ 
+  # def hp=(hp)
+  #   @hp = hp
+  # end
 
-  #hpのゲッター
+ 
   def hp
     @hp
   end
 
-  #offenseのセッター
-  def offense=(offense)
-    @offense = offense
-  end
+  
+  # def offense=(offense)
+  #   @offense = offense
+  # end
 
-  #offenseのゲッター
+  
   def offense
     @offense
   end
 
-  #defenseのセッター
-  def defense=(defense) 
-    @defense = defense
-  end   
+  
+  # def defense=(defense) 
+  #   @defense = defense
+  # end   
 
-  #defenseのゲッター
+  
   def defense
     @defense
   end
 
   brave = Brave.new
-  brave.name = "テリー"
-  brave.hp = 500
-  brave.offense = 150
-  brave.defense = 100
+  # インスタンス作成時にinitializeメソッドが呼び出され初期値を設定するため以下不要のためを削除
+  # brave.name = "テリー"
+  # brave.hp = 500
+  # brave.offense = 150
+  # brave.defense = 100
+
 
   # putsや文字列を囲む""（ダブルクウォーテーション）を何度も書く必要がなくなります。
   # 複数行の文字列を書くときにはこちらの方が楽なのでぜひ使ってみてください。
