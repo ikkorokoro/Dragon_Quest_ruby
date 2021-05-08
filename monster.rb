d74717c947303de33cacf896c7b8c5010376574b
今吉恒介 <ikkorokoro@icloud.com>
2021-05-08 23:33:34 +0900

    必殺技 メソッド追加

--- monster.rb
+++ monster.rb
@@ -1,7 +1,10 @@
 class Brave
   attr_reader :name, :offense, :defense
   attr_accessor :hp
-  
+
+  # 必殺攻撃の計算に使う定数
+  SPECIAL_ATTACK_CONSTANT = 1.5
+
   def initialize(params)
     @name = params[:name]
     @hp = params[:hp]
@@ -10,17 +13,34 @@ class Brave
   end
 
   def attack(monster)
-    damage = @offense - monster.defense
+    puts "#{@name}の攻撃"
+
+    # 0~3の間でランダムに数字が変わる
+    attack_num = rand(4)
+
+    # 4分の1の確率でspecial_attackを実行
+    if attack_num == 0
+      puts "必殺攻撃"
+      # calculate_special_attackの呼び出し
+      damage = calculate_special_attack - monster.defense
+    else
+      puts "通常攻撃"
+      damage = @offense - monster.defense
+    end
+
     # ダメージを与えられたHPをモンスターインスタンスのHPに代入(自己代入記法)
     monster.hp -= damage
+
     # メッセージを追記
-    puts "#{@name}の攻撃"
     puts "#{monster.name}は#{damage}のダメージを受けた"
     puts "#{monster.name}の残りHPは#{monster.hp}だ"
   end
+
+  def calculate_special_attack
+    # 攻撃力が1.5倍
+    @offense * SPECIAL_ATTACK_CONSTANT
+  end
 end
-
-
 
 class Monster
   attr_reader :name, :offense, :defense
