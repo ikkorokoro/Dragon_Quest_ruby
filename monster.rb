ebf0404b0b8c8b91c283664c35318f39a84523ea
今吉恒介 <ikkorokoro@icloud.com>
2021-05-12 23:26:25 +0900

    変身メソッドを定義

--- monster.rb
+++ monster.rb
@@ -46,15 +46,39 @@ class Monster
   attr_reader :name, :offense, :defense
   attr_accessor :hp
 
+  POWER_UP_RATE = 1.5
+  CALC_HALF_HP = 0.5
+
   def initialize(**params)
     @name = params[:name]
     @hp = params[:hp]
     @offense = params[:offense]
-    @defense = params[:defense]   
+    @defense = params[:defense]
+
+    # モンスターが変身したかどうかを判定するフラグ
+      @transform_flag == false
+    # 変身する際の閾値（トリガー）を計算
+      @trigger_of_transform = params[:hp] * CALC_HALF_HP
+    
+    def attack(brave)
+      if @hp <= @trigger_of_transform && @transform_flag == false
+        @transform_flag == true
+        transtorm
+      end
+      puts "#{@name}の攻撃"
+      damage = @offense - brave.defense
+      brave.hp -= damage
+    end
+  end
+
+  private
+
+  def transform
+    
   end
 end
   
   monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
   brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
   brave.attack(monster)
-  
+  monster.attack(brave)
