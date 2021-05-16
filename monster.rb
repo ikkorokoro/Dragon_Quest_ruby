5d71afc6a6d265169fa26bd48c4f82bdd894242e
今吉恒介 <ikkorokoro@icloud.com>
2021-05-16 18:41:39 +0900

    モンスターの攻撃処理を実装

--- monster.rb
+++ monster.rb
@@ -43,8 +43,10 @@ class Brave
 end
 
 class Monster
-  attr_reader :name, :offense, :defense
-  attr_accessor :hp
+  # nameを削除
+  attr_reader :offense, :defense
+  # nameを追加
+  attr_accessor :hp, :name
 
   POWER_UP_RATE = 1.5
   CALC_HALF_HP = 0.5
@@ -55,29 +57,39 @@ class Monster
     @offense = params[:offense]
     @defense = params[:defense]
 
-    # モンスターが変身したかどうかを判定するフラグ
-      @transform_flag == false
-    # 変身する際の閾値（トリガー）を計算
-      @trigger_of_transform = params[:hp] * CALC_HALF_HP
-    
-    def attack(brave)
-      if @hp <= @trigger_of_transform && @transform_flag == false
-        @transform_flag == true
-        transtorm
-      end
-      puts "#{@name}の攻撃"
-      damage = @offense - brave.defense
-      brave.hp -= damage
+    @transform_flag = false
+    @trigger_of_transform = params[:hp] * CALC_HALF_HP
+  end
+
+  def attack(brave)
+    if @hp <= @trigger_of_transform && @transform_flag == false
+      @transform_flag = true
+      transform
     end
+    puts "#{@name}の攻撃"
+
+    damage = @offense - brave.defense
+    brave.hp -= damage
+
+    puts "#{brave.name}は#{damage}のダメージを受けた"
+    puts "#{brave.name}の残りHPは#{brave.hp}だ"
   end
 
   private
 
-  def transform
-    
-  end
+    def transform
+      transform_name = "ドラゴン"
+
+      puts <<~EOS
+      #{@name}は怒っている
+      #{@name}は#{transform_name}に変身した
+      EOS
+
+      @offense *= POWER_UP_RATE
+      @name = transform_name
+    end
+
 end
-  
   monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
   brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
   brave.attack(monster)
