0ec7d3bf00d6ca42529195384c13d1c767dc1c75
今吉恒介 <ikkorokoro@icloud.com>
2021-05-25 17:41:14 +0900

    リファクタリング

--- games_controller.rb
+++ games_controller.rb
@@ -1,46 +1,56 @@
 class GamesController
   EXP_CONSTANT = 2
   GOLD_CONSTANT = 3
+  
   #バトル処理
   def battle(**params)
-    brave = params[:brave]
-    monster = params[:monster]
+    build_caracters(params)
 
     loop do
-      brave.attack(monster)
-      break if battle_end?(monster)
-      monster.attack(brave)
-      break if battle_end?(brave)
+      @brave.attack(@monster)
+      break if battle_end?
+      @monster.attack(@brave)
+      break if battle_end?
     end
 
     #勇者の勝敗によってメッセージを変える
-    if battle_result(brave)
-      result = calculate_of_exp_and_gold(monster)
-      puts "#{brave.name}は戦いに勝った"
-      puts "#{result[:exp]}の経験値と#{result[:gold]}ゴールドを獲得した"
-    else
-      puts "#{brave.name}はたたかいに負けた"
-      puts "目の前が真っ暗になった"
-    end
+    battle_jugment
   end
 
   # 以下のメソッドはクラス外から呼び出す必要がないのでprivate以下に記述する
   private
+  #メソッド間の引数の受け渡しをなくすため、インスタンス変数に代入して参照できるようにする
+  def build_caracters
+    @brave = params[:brave] don’t 
+    @monster = @params[:monster]
+  end
 
   #バトル終了の判定
-  def battle_end?(character)
-    character.hp <= 0
+  def battle_end?
+    #勇者かモンスターどちらかがhpが0になったらバトルを終了する
+    @brave.hp <= 0 || @monster.hp <= 0 
   end
 
   #勇者の勝利判定
-  def battle_result(brave)
-    brave.hp > 0
+  def brave_win?
+    @brave.hp > 0
+  end
+
+  def battle_jugment
+    if brave_win?
+      result = calculate_of_exp_and_gold
+      puts "#{@brave.name}は戦いに勝った"
+      puts "#{result[:exp]}の経験値と#{result[:gold]}ゴールドを獲得した"
+    else
+      puts "#{@brave.name}はたたかいに負けた"
+      puts "目の前が真っ暗になった"
+    end
   end
   
   #経験値とゴールドの計算
-  def calculate_of_exp_and_gold(monster)
-    exp = (monster.offense + monster.defense) * EXP_CONSTANT
-    gold = (monster.offense + monster.defense) * GOLD_CONSTANT
+  def calculate_of_exp_and_gold
+    exp = (@monster.offense + @monster.defense) * EXP_CONSTANT
+    gold = (@monster.offense + @monster.defense) * GOLD_CONSTANT
     result = {exp: exp, gold: gold}
 
     result
